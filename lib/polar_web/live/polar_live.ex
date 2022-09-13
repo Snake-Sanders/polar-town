57249fc31f2ae72d38ec513b15c7bb6df00a4d2f
Snake Sanders <elmarian22@gmail.com>
2022-09-13 14:30:33 +0200

    added style to buttons

--- lib/polar_web/live/polar_live.ex
+++ lib/polar_web/live/polar_live.ex
@@ -7,7 +7,13 @@ defmodule PolarWeb.PolarLive do
 
   def render(assigns) do
     ~H"""
-    <h1 class="text-4x1 font-bold text-center text-indigo-600">Polar</h1>
+    <h1 class="text-4x1 text-xl font-bold text-center text-blue-800">Polar Services</h1>
+    <!-- <div class="flex flex-row items-stretch"> -->
+    <div class="flex justify-around mt-6">
+      <div class="bg-blue-200 p-3 font-bold text-indigo-800 rounded-tr-lg">Vehicle</div>
+      <div class="bg-blue-200 p-3 font-bold text-indigo-800 rounded-tr-lg">Parking</div>
+      <div class="bg-blue-200 p-3 font-bold text-indigo-800 rounded-tr-lg">Sauna</div>
+    </div>
     """
   end
 end
