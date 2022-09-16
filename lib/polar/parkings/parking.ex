cc42e46cc7547bd8ef524b0af68ee5623d3312a8
Snake Sanders <elmarian22@gmail.com>
2022-09-16 12:35:48 +0200

    clean up logs

--- lib/polar/parkings/parking.ex
+++ lib/polar/parkings/parking.ex
@@ -20,6 +20,5 @@ defmodule Polar.Parkings.Parking do
     |> validate_required([:name])
     |> validate_number(:lat, greater_than_or_equal_to: 0.0)
     |> validate_number(:lng, greater_than_or_equal_to: 0.0)
-    |> IO.inspect(label: "changeset!!!!")
   end
 end
