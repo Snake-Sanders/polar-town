3ae9899b391729b386a0ee5046c1ad59557a70bf
Snake Sanders <elmarian22@gmail.com>
2022-09-16 15:44:44 +0200

    added parking description display

--- lib/polar_web/live/polar_live.ex
+++ lib/polar_web/live/polar_live.ex
@@ -70,14 +70,27 @@ defmodule PolarWeb.PolarLive do
   def render(assigns) do
     ~H"""
     <h1 class="text-center">Polar Services</h1>
-
-    <div class="flex justify-around mt-6">
-      <div>
-        <%= live_redirect("Admin Parkings", to: "/parkings", class: "button") %>
-      </div>
+    <div class="flex justify-end">
+      <%= live_redirect("Admin Parkings", to: "/parkings", class: "button") %>
     </div>
 
-    <div class="flex pt-16">
+    <div class="mt-6 w-full border border-gray-300 rounded-xl p-2 bg-white">
+      <%= if is_nil(@selected_item) do %>
+        <p>Select a Parking from the list</p>
+      <% else %>
+        <div class="flex flex-row space-x-8 p-4 items-center justify-center ">
+          <div class="">
+            <%= display_parking_info(@selected_item)%>
+          </div>
+
+          <div class="">
+            <%= live_redirect("Reserve now!", to: "/parkings", class: "button") %>
+          </div>
+        </div>
+      <% end %>
+    </div>
+
+    <div class="flex pt-8">
 
       <div id="map-wrapper" phx-update="ignore" class="grow h-380">
         <div id="map"
@@ -98,5 +111,24 @@ defmodule PolarWeb.PolarLive do
 
   defp find_parking(socket, parking_id) do
     Enum.find(socket.assigns.parkings, fn p -> p.id == parking_id end)
+  end
+
+  defp display_parking_info(parking) do
+    assigns = %{parking: parking}
+
+    ~H"""
+    <ul>
+      <li><h3 class="mt-0">Parking description</h3></li>
+      <li><p class="font-semibold">
+        Location: <span class="font-normal"><%= @parking.name %></span>
+        </p></li>
+      <li><p class="font-semibold">
+        Electric charger: <span class="font-normal"><%= if @parking.has_charger, do: "Yes", else: "No" %></span>
+        </p></li>
+      <li><p class="font-semibold">
+        Status: <span class="font-normal"><%= if @parking.is_free, do: "Available now", else: "Not available" %></span>
+        </p></li>
+    </ul>
+    """
   end
 end
