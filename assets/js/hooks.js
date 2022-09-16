79f85419163e625885a9a4566f77aac5cfc0ee81
Snake Sanders <elmarian22@gmail.com>
2022-09-16 11:08:01 +0200

    added comments

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -8,6 +8,7 @@ let Hooks = {};
 Hooks.PhxHookGeoAssetMap = {
     mounted() {
         console.log("GeoAssetMap mounted");
+
         this.map = new GeoAssetMap(
             this.el, [68.2326783,14.5468427], event => {
                 const geoassetId = event.target.options.geoassetId;
@@ -17,17 +18,20 @@ Hooks.PhxHookGeoAssetMap = {
             }
         )
 
-        // now the client request the geoassets to the server
+        // When mounted, it sends a requests to the server
+        // asking for the list of geoassets.
         this.pushEvent("get-geoassets", {}, (reply, ref) => {
             reply.geoassets.forEach(geoasset => {
                 this.map.addMarker(geoasset)
             })
         })
 
+        // The server requests to highlight a marker on the map.
         this.handleEvent("highlight-marker", geoasset => {
             this.map.highlightMarker(geoasset)
         })
 
+        // The server request to add a marker on the map.
         this.handleEvent("add-marker", geoasset => {
             this.map.addMarker(geoasset);
             this.map.highlightMarker(geoasset);
