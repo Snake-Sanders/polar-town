a5c458271de1006ffad4814119c9dbf412aa056c
Snake Sanders <elmarian22@gmail.com>
2022-09-18 15:39:05 +0200

    migrated parking-reservations

--- priv/repo/migrations/20220917172825_create_parking_reservations.exs
+++ priv/repo/migrations/20220917172825_create_parking_reservations.exs
@@ -1,12 +1,15 @@
 defmodule Polar.Repo.Migrations.CreateParkingReservations do
+
+  # mix phx.gen.live Reservations ParkingReservation parking_reservations parking_id:references:parkings user_id:references:users time_start:naive_datetime time_end:naive_datetime
+
   use Ecto.Migration
 
   def change do
     create table(:parking_reservations) do
       add :time_start, :naive_datetime
       add :time_end, :naive_datetime
-      add :parking_id, references(:parkings, on_delete: :nothing)
-      add :user_id, references(:users, on_delete: :nothing)
+      add :parking_id, references(:parkings, on_delete: :delete_all)
+      add :user_id, references(:users, on_delete: :delete_all)
 
       timestamps()
     end
